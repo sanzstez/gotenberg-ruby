45f92bb2b6aaed73d2b761af42bc43350613a81b
Alexandr Stets <sanzstez@gmail.com>
2026-07-12 01:12:31 +0300

    Add Chromium network idle property tests

---
+++ test/gotenberg/chromium/properties_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'gotenberg/chromium/properties'
+
+class ChromiumPropertiesTest < Minitest::Test
+  class Harness
+    include Gotenberg::Chromium::Properties
+
+    def form_properties
+      send(:properties)
+    end
+  end
+
+  def setup
+    @document = Harness.new
+  end
+
+  def test_wait_for_network_idle_enables_strict_network_idle_wait
+    result = @document.wait_for_network_idle
+
+    assert_same @document, result
+    assert_equal false, @document.form_properties['skipNetworkIdleEvent']
+  end
+
+  def test_wait_for_network_almost_idle_enables_network_almost_idle_wait
+    result = @document.wait_for_network_almost_idle
+
+    assert_same @document, result
+    assert_equal false, @document.form_properties['skipNetworkAlmostIdleEvent']
+  end
+end
