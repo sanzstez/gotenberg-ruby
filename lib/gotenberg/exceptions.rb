509736946be70ecf463478c9e3808f007a1a8971
Andrey Moshkov <fakesupp@gmail.com>
2026-07-23 17:30:33 +0300

    Expose response details on transform errors

--- lib/gotenberg/exceptions.rb
+++ lib/gotenberg/exceptions.rb
@@ -1,4 +1,25 @@
 module Gotenberg
-  class TransformError < StandardError; end
+  class TransformError < StandardError
+    RESPONSE_ATTRIBUTES = %i[
+      response
+      response_status
+      response_headers
+      response_body
+    ].freeze
+
+    attr_reader :original_error
+
+    def initialize(original_error = nil)
+      @original_error = original_error
+      super
+    end
+
+    RESPONSE_ATTRIBUTES.each do |attribute|
+      define_method attribute do
+        original_error.public_send(attribute) if original_error.respond_to?(attribute)
+      end
+    end
+  end
+
   class RemoteSourceError < StandardError; end
 end
